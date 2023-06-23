b49591d8183b5a7e0fa7bacb767c7ec60e7c2b65
chethaslp <lchethas@gmail.com>
2023-06-23 06:10:40 +0530

    v2.0

---
+++ config.py
@@ -0,0 +1,7 @@
+
+CRED_FILE = "cred.json"
+STORAGE_URL = {'storageBucket': 'examerapp.appspot.com',
+                'databaseURL':'https://examerapp.firebaseio.com'}
+PASSWORD = "chethas123"
+VIEW_URL = "/view/clp"
+SECRET_KEY = "chcnjshcbsjyubv8"
--- web.py
+++ web.py
@@ -1,40 +1,81 @@
-from flask import Flask, render_template, request, redirect ,url_for
+from flask import Flask, render_template, request, redirect ,url_for, session
 import firebase_admin
 from firebase_admin import credentials, storage, db
 import time
 import json
+from config import *
+
+"""
+DROPPER
+ ~CLP - 2023
+
+ Dropper is a flask app that allows users to upload files and share messages with themselves.
+ Dropper is intented for personal use.
+
+ Prereqs:
+   * Create a Firebase project and download your credentials as json.
+   * IN config.py ADD YOUR CREDENTIALS AND PASSWORD.
+   * To host on web, you should have a hosting service with python support.
+"""
 
 app = Flask(__name__)
-cred = credentials.Certificate("cred.json")
-firebase_admin.initialize_app(cred,{'storageBucket': 'examerapp.appspot.com','databaseURL':'https://examerapp.firebaseio.com'})
+cred = credentials.Certificate(CRED_FILE)
+firebase_admin.initialize_app(cred,STORAGE_URL)
+app.secret_key = SECRET_KEY
+pw = PASSWORD
 
+def init():
+    if db.reference("dropper/pw").get(): pw = db.reference("dropper/pw").get()
+    print(pw)
 
 @app.route("/drop/upload",methods=["POST"])
 def upload():
+    """ Upload endpoint.
+            * Uploads files to firebase storage.
+    """
     f = request.files['file']
     fn = f.filename
     if not fn == '':
-        blb = storage.bucket().blob('drops/'+str(int(time.time()))+"_"+fn)
+        blb = storage.bucket().blob('dropper/'+str(int(time.time()))+"_"+fn)
         blb.upload_from_file(f,content_type=f.content_type)
         blb.make_public()
         return blb.public_url
     return "False"
 
+@app.route("/drop/chng_pw",methods=["POST"])
+def chng_pw():
+    """ Change Password endpoint.
+    """
+    if request.form['crt_pw'] == pw:
+        if request.form['new_pw'] == request.form['new_pwc']:
+            db.reference("dropper/pw").set(request.form['new_pw'])
+            init()
+            return "True"
+    return "False"
+
 @app.route("/",methods=['GET','POST'])
 def root():
+    """ Root
+            * Renders the home page.
+    """
     if request.method == 'POST':
-        d = db.reference("drops/")
+        d = db.reference("dropper/drops/")
         if request.form['fl'] == "": d.child(str(int(time.time()))).set({'d':request.form['msg'],"ip":request.remote_addr})
         else: d.child(str(int(time.time()))).set({'d':request.form['msg'],'f':request.form['fl'],"ip":request.remote_addr})
         return redirect("/")
     return render_template("index.html")
 
-@app.route("/view/clp",methods=['POST','GET'])
+@app.route(VIEW_URL,methods=['POST','GET'])
 def vvsd():
-    if request.method=='POST':
-        if request.form['pw'] == "chethas123":
-            d = db.reference("drops/").get()
-            return render_template("view.html",login=True,d=d)
+    """ View 
+            * Allows user to access the shared datas."""
+    init()
+    if session.get("pw") and session.get("pw") == pw:
+        d = db.reference("dropper/drops/").get()
+        return render_template("view.html",login=True,d=d)
+    if request.method=='POST' and request.form['pw'] == pw: 
+        session["pw"] = pw
+        return redirect(VIEW_URL)
     return render_template("view.html",login=False)
 
 @app.template_filter('json')
@@ -43,7 +84,7 @@ def from_from(st):
 
 @app.template_filter('fn')
 def getfn(st):
-    return st.replace("https://storage.googleapis.com/examerapp.appspot.com/drops/","").split("_",1)[1]
+    return st.replace("https://storage.googleapis.com/"+STORAGE_URL["storageBucket"]+"/dropper/","").split("_",1)[1]
 
 @app.template_filter('dt')
 def dt(time=False):
